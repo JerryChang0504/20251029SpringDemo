ce8db5571406d7d2a388474d29b7dd1c07baa7a4
DESKTOP-P4UUKAO\student <redcarbb@gmail.com>
2025-10-29 21:19:41 +0800

    add product.java

---
+++ src/main/java/om/gjun/jds/entity/Product.java
@@ -0,0 +1,30 @@
+package om.gjun.jds.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Data
+@Builder
+@Table(name = "Product")
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "product_id")
+	private Integer Id;
+	@Column(name = "product_name")
+	private String name;
+	@Column(name = "product_description")
+	private String description;
+	@Column(name = "product_price")
+	private Integer product_price;
+	private Integer image;
+
+}
