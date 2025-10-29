6fcb69d52e1b20d4a45b9265eb094813b52baf77
hzn <hzn>
2025-10-29 21:53:45 +0800

    10/29 last commit

---
+++ src/main/java/om/gjun/jds/entity/BaseEntity.java
@@ -0,0 +1,17 @@
+package om.gjun.jds.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.Column;
+
+public class BaseEntity {
+	@CreatedDate // 當 Entity 第一次儲存時自動填入時間
+	@Column(updatable = false) // 此欄位建立後不可被更新
+	protected LocalDateTime createdAt;
+
+	@LastModifiedDate // 每次更新時會自動填入目前時間
+	protected LocalDateTime updatedAt;
+}
--- src/main/java/om/gjun/jds/entity/Product.java
+++ src/main/java/om/gjun/jds/entity/Product.java
@@ -14,7 +14,7 @@ import lombok.Data;
 @Builder
 @Table(name = "product")
 public class Product {
-//
+
 	// field
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -32,46 +32,5 @@ public class Product {
 
 	@Column(name = "product_image")
 	private String image;
-
-	// Getter & Setter
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public Integer getPrice() {
-		return price;
-	}
-
-	public void setPrice(Integer price) {
-		this.price = price;
-	}
-
-	public String getImage() {
-		return image;
-	}
-
-	public void setImage(String image) {
-		this.image = image;
-	}
 
 }
