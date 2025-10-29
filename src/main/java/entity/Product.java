6cac9dba890a8a5b1cbbf37d1967d441d69221bd
GOTO123978 <a0981640016@gmail.com>
2025-10-29 21:11:04 +0800

    add Product @entity

---
+++ src/main/java/entity/Product.java
@@ -0,0 +1,21 @@
+package entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Entity
+@Data
+public class Product {
+
+	  @Column(name = "product_id")
+	  private Integer id;
+	  @Column(name = "product_name")
+	  private String name;
+	  @Column(name = "product_description")
+	  private String description;
+	  @Column(name = "product_price")
+	  private Integer price;
+	  @Column(name = "product_image")
+	  private String image;
+}
