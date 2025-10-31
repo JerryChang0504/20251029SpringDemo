07148322ab50b362686ac14b5914b0a4ebd0bb8d
DESKTOP-P4UUKAO\student <kim1475369h@gmail.com>
2025-10-31 21:52:21 +0800

    add swagger add SwaggerConfig.java&swagger@

--- src/main/java/om/gjun/jds/controller/ProductController.java
+++ src/main/java/om/gjun/jds/controller/ProductController.java
@@ -6,15 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import om.gjun.jds.dto.request.ProductReq;
 import om.gjun.jds.dto.request.UpdateProductReq;
 import om.gjun.jds.entity.Product;
 import om.gjun.jds.sevice.ProductService;
 
+@Tag(name = "產品操作", description = "提供產品新增、查詢等操作")
 @RestController
 @RequestMapping("api")
 public class ProductController {
@@ -22,6 +27,8 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 
+	@Operation(summary = "取得所有產品清單")
+	@ApiResponse(responseCode = "200", description = "查詢成功")
 	@GetMapping("queryAllProducts")
 	public ResponseEntity<List<Product>> queryAllProducts() {
 		return ResponseEntity.ok(productService.queryAllProducts());
@@ -32,7 +39,7 @@ public class ProductController {
 		ResponseEntity.ok(productService.saveProduct(req));
 	}
 
-	@PostMapping("updateProduct")
+	@PutMapping("updateProduct")
 	public Product postMethodName(@RequestBody UpdateProductReq req) {
 		return productService.updatProduct(req);
 	}
