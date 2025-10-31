36d9d75e3b5303c5da003a4f472b6c53d8196ace
DESKTOP-P4UUKAO\student <kim1475369h@gmail.com>
2025-10-31 21:19:37 +0800

    updateProduct

--- src/main/java/om/gjun/jds/dto/request/UpdateProductReq.java
+++ src/main/java/om/gjun/jds/dto/request/UpdateProductReq.java
@@ -3,8 +3,8 @@ package om.gjun.jds.dto.request;
 import lombok.Data;
 
 @Data
-public class ProductReq {
-
+public class UpdateProductReq {
+	private Integer id;
 	private String name;
 	private String description;
 	private Integer price;
--- src/main/java/om/gjun/jds/sevice/ProductService.java
+++ src/main/java/om/gjun/jds/sevice/ProductService.java
@@ -3,6 +3,7 @@ package om.gjun.jds.sevice;
 import java.util.List;
 
 import om.gjun.jds.dto.request.ProductReq;
+import om.gjun.jds.dto.request.UpdateProductReq;
 import om.gjun.jds.entity.Product;
 
 public interface ProductService {
@@ -10,4 +11,6 @@ public interface ProductService {
 	public List<Product> queryAllProducts();
 
 	public Product saveProduct(ProductReq req);
+
+	public Product updatProduct(UpdateProductReq req);
 }
