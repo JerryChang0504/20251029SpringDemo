6be62092bd8af929b0f05b25a10dd8aa511e4e3c
Jerry Chang <fx2ptt@gmail.com>
2025-10-31 21:24:05 +0800

    安裝Swagger

--- src/main/java/om/gjun/jds/controller/ProductController.java
+++ src/main/java/om/gjun/jds/controller/ProductController.java
@@ -1,15 +1,14 @@
 package om.gjun.jds.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import om.gjun.jds.dto.request.ProductReq;
 import om.gjun.jds.dto.request.UpdateProductReq;
 import om.gjun.jds.entity.Product;
@@ -19,22 +18,22 @@ import om.gjun.jds.servie.ProductService;
 @RequestMapping("api")
 public class ProductController {
 
-	@Autowired
-	private ProductService productService;
+  @Autowired
+  private ProductService productService;
 
-	@GetMapping("queryAllProducts")
-	public ResponseEntity<List<Product>> queryAllProducts() {
-		return ResponseEntity.ok(productService.queryAllProducts());
-	}
+  @GetMapping("queryAllProducts")
+  public ResponseEntity<List<Product>> queryAllProducts() {
+    return ResponseEntity.ok(productService.queryAllProducts());
+  }
 
-	@PostMapping("saveProducts")
-	public void saveProduct(@RequestBody ProductReq req) {
-		ResponseEntity.ok(productService.saveProduct(req));
-	}
+  @PostMapping("saveProducts")
+  public void saveProduct(@RequestBody ProductReq req) {
+    ResponseEntity.ok(productService.saveProduct(req));
+  }
 
-	@PostMapping("updateProduct")
-	public Product updateProduct(@RequestBody UpdateProductReq req) {
-		return productService.updateProduct(req);
-	}
+  @PutMapping("updateProduct")
+  public Product updateProduct(@RequestBody UpdateProductReq req) {
+    return productService.updateProduct(req);
+  }
 
 }
