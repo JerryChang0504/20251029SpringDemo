bc69c8526cb784e28ea9bcc4f496490cd888e8c4
GOTO123978 <a0981640016@gmail.com>
2025-11-03 21:56:03 +0800

    modify CORS & annotation name

--- src/main/java/com/gjun/controller/ProductController.java
+++ src/main/java/com/gjun/controller/ProductController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.gjun.dto.request.ProductReq;
 import com.gjun.entity.Product;
 import com.gjun.service.ProductService;
-
+@CrossOrigin(origins = "http://localhost:5175")
 @RestController
 @RequestMapping("api")
 public class ProductController {
@@ -21,7 +22,7 @@ public class ProductController {
 	@Autowired
 	private ProductService  productService;
 	
-	@GetMapping("QueryProduct")
+	@GetMapping("QueryAllProducts")
 	public ResponseEntity<List<Product>> QueryProduct(){
 		return ResponseEntity.ok(productService.QueryAllProduct());
 	}
