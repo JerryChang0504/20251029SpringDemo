d47b9a4d6e3b2262f8496b649913f7a12622bc5a
GOTO123978 <a0981640016@gmail.com>
2025-11-05 20:47:06 +0800

    modify

--- src/main/java/com/gjun/controller/ProductController.java
+++ src/main/java/com/gjun/controller/ProductController.java
@@ -9,11 +9,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gjun.dto.request.ProductReq;
+import com.gjun.dto.request.UpdateProductReq;
 import com.gjun.entity.Product;
 import com.gjun.service.ProductService;
 
@@ -28,6 +30,8 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 
+	@Operation(summary = "取得所有產品清單")
+	@ApiResponse(responseCode = "200", description = "查詢成功")
 	@GetMapping("queryAllProducts")
 	public ResponseEntity<List<Product>> QueryProduct() {
 		return ResponseEntity.ok(productService.QueryAllProduct());
@@ -36,6 +40,11 @@ public class ProductController {
 	@PostMapping("saveProducts")
 	public void saveProduct(@RequestBody ProductReq req) {
 		ResponseEntity.ok(productService.saveProduct(req));
+	}
+
+	@PutMapping("updateProduct")
+	public Product updateProduct(@RequestBody UpdateProductReq req) {
+		return productService.updateProduct(req);
 	}
 
 	@Operation(summary = "刪除產品")
